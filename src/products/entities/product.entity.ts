96bacfcb4de7b85fcb15c9444a3bb4bfcfdf056d
Duvan Roque <dsmith.mr@gmail.com>
2024-03-20 11:25:09 -0500

    BeforeUpdate

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Product {
@@ -86,6 +86,14 @@ export class Product {
             this.slug = this.title;
         }
 
+        this.slug = this.slug
+            .toLowerCase()
+            .replaceAll(' ', '_')
+            .replaceAll("'", '');
+    }
+
+    @BeforeUpdate()
+    checkSlugUpdate() {
         this.slug = this.slug
             .toLowerCase()
             .replaceAll(' ', '_')
