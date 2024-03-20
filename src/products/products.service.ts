feb7af7f17c2d2080b51e1a335cbce48b098ea3c
Duvan Roque <dsmith.mr@gmail.com>
2024-03-20 09:48:14 -0500

    findOne where

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -44,8 +44,14 @@ export class ProductsService {
       if (isUUID(term))
         product = await this.productRepository.findOneBy({ id: term });
 
-      if (!product)
-        product = await this.productRepository.findOneBy({ slug: term });
+      if (!product) {
+        const queryBuilder = this.productRepository.createQueryBuilder();
+        product = await queryBuilder
+          .where(`UPPER(title) =:title or slug =:slug`, {
+            title: term.toUpperCase(),
+            slug: term.toLowerCase(),
+          }).getOne();
+      }
 
       if (!product)
         throw new NotFoundException(`product with "${term}" not found`);
