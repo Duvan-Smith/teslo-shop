3a86e194bb545a923ab56686eec87efb88dd5d82
Duvan Roque <dsmith.mr@gmail.com>
2024-03-24 18:54:38 -0500

    Conexion Seed con producto

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -153,4 +153,17 @@ export class ProductsService {
       images: images.map(image => image.url)
     }
   }
+
+  async deleteAllProucts() {
+    const query = this.productRepository.createQueryBuilder('product');
+
+    try {
+      return await query
+        .delete()
+        .where({})
+        .execute();
+    } catch (error) {
+      this.handleExecptions(error);
+    }
+  }
 }
