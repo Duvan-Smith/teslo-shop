21fc4a2f4b81fd19e0efa379e6b12cab7ba18893
Duvan Roque <dsmith.mr@gmail.com>
2024-03-26 17:56:18 -0500

    env

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
+  console.log(`App running on port ${process.env.PORT}`);
 }
 
 bootstrap();
